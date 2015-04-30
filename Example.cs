1cdc0d8b912628ffcc66bd307400a22391f957ea
Sarbian <Sarbian@sarbian.com>
2015-04-30 21:55:37 +0200

    Include a simple example code

---
+++ Example.cs
@@ -0,0 +1,28 @@
+﻿using ModularFI;
+using UnityEngine;
+
+namespace MyFunMod
+{
+    // this is a really simple example of how to use ModularFlightIntegrator
+    // This one would replace the drag calculation to remove drag whole
+    // Obviously you would have to uncomment the lines for it to work.
+    
+    /*
+
+    [KSPAddon(KSPAddon.Startup.SpaceCentre, false)]
+    class Example : MonoBehaviour
+    {
+
+        public void Awake()
+        {
+            ModularFI.ModularFlightIntegrator.RegisterCalculateDragValueOverride(IDontLikeDrag);
+        }
+
+
+        private static double IDontLikeDrag(ModularFlightIntegrator fi, Part part)
+        {
+            return 0;
+        }
+    }
+    */
+}
