d2ac4570ef677935f5cbe9a67627b9808fb6743c
Sarbian <Sarbian@sarbian.com>
2016-06-25 20:04:27 +0200

    v1.1.6 to fix the CKAN mess

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -33,7 +33,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.1.5.0")]
+[assembly: AssemblyFileVersion("1.1.6.0")]
 
 
 [assembly: KSPAssembly("ModularFlightIntegrator", 1, 0)]
